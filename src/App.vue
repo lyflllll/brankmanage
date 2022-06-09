c2c51d5f6a2f4124e3911756f3564c6e9627af99
lyf <2671700148@qq.com>
2022-06-09 12:16:02 +0800

    品牌管理

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>品牌管理</h1>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>资产名称</th>
+          <th>价格</th>
+          <th>创建时间</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="txt in list" :key="txt.id">
+          <td>{{txt.id}}</td>
+          <td>{{txt.name}}</td>
+          <td v-if="txt.price>100" :style="{color:'red'}">{{txt.price}}</td>
+          <td v-else>{{txt.price}}</td>
+          <td>{{txt.time}}</td>
+          <td>{{del}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      list: [
+        { id: 100, name: "外套", price: 199, time: new Date('2010-08-12')},
+        { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
+        { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
+        { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
+      ],
+      del:'删除'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
